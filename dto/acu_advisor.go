09c11613702d9e094426e91c5c4ab0c2e440db06
wzr0012009 <9270085+jerrypsy@user.noreply.gitee.com>
2026-08-23 00:07:50 +0200

    style: format private acu advisor dto

--- dto/acu_advisor.go
+++ dto/acu_advisor.go
@@ -1,21 +1,21 @@
 package dto
 
 type ACUAdvisor struct {
-	AdvisorID         string                 `json:"advisorId"`
-	NewAPIUserID      string                 `json:"newapiUserId"`
-	LogicalRequestID  string                 `json:"logicalRequestId"`
-	TriggerCallCount  int64                  `json:"triggerCallCount"`
-	NeedAdvisor       bool                   `json:"needAdvisor"`
-	Status            string                 `json:"status"`
-	Problem           string                 `json:"problem"`
-	Advice            string                 `json:"advice,omitempty"`
-	Learn             string                 `json:"learn"`
-	RelevantSkillIDs  []string               `json:"relevantSkillIds"`
-	CreatedAt         string                 `json:"createdAt"`
-	UserFeedback      string                 `json:"userFeedback,omitempty"`
-	FeedbackAt        string                 `json:"feedbackAt,omitempty"`
-	ObserverResult    map[string]interface{} `json:"observerResult,omitempty"`
-	AdvisorResult     map[string]interface{} `json:"advisorResult,omitempty"`
+	AdvisorID        string                 `json:"advisorId"`
+	NewAPIUserID     string                 `json:"newapiUserId"`
+	LogicalRequestID string                 `json:"logicalRequestId"`
+	TriggerCallCount int64                  `json:"triggerCallCount"`
+	NeedAdvisor      bool                   `json:"needAdvisor"`
+	Status           string                 `json:"status"`
+	Problem          string                 `json:"problem"`
+	Advice           string                 `json:"advice,omitempty"`
+	Learn            string                 `json:"learn"`
+	RelevantSkillIDs []string               `json:"relevantSkillIds"`
+	CreatedAt        string                 `json:"createdAt"`
+	UserFeedback     string                 `json:"userFeedback,omitempty"`
+	FeedbackAt       string                 `json:"feedbackAt,omitempty"`
+	ObserverResult   map[string]interface{} `json:"observerResult,omitempty"`
+	AdvisorResult    map[string]interface{} `json:"advisorResult,omitempty"`
 }
 
 type ACUAdvisorList struct {
